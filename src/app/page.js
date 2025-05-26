07d001f19fa9b9214f846b287e3d50e7887dfb93
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-25 19:33:41 -0500

    add: welcome home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import TestAuth from "@/components/TextAuth";
 
 export default function Home() {
   return (
     <>
-      <main></main>
+      <main>
+        <h1>Welcome</h1>
+      </main>
     </>
   );
 }

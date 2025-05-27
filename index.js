d3e00b049b9210d648267de19d026862fb02fdf2
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-27 17:29:07 -0500

    finish testing

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const bookingRoutes = require("./src//routes/bookingRoutes");
+
+app.use(express.json());
+app.use("/api/bookings", bookingRoutes);
+
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
---
+++ src/routes/bookingRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+const createBookings = (req, res) => {
+  res.send("Created bookings");
+};
+
+const getMyBookings = (req, res) => {
+  res.send("Get my bookings");
+};
+
+const getBookingsById = (req, res) => {
+  res.send("Get bookings by id");
+};
+
+const cancelBookings = (req, res) => {
+  res.send("Cancel my bookings");
+};
+router.post("/", createBookings);
+router.get("/my-bookings", getMyBookings);
+router.get("/:id", getBookingsById);
+router.delete("/:id", cancelBookings);
+module.export = router;

dc7138aa227d29bd9dc27f70bcee5fc37b258c49
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-27 12:02:47 -0500

    chore: use dotenv to load MongoDB URI in mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://gregoryrobertsondev:<db_password>@cluster0.dkpddoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,9 +17,7 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Connected to MongoDB successfully!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();

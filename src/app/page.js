473d83e355f7b68eb0bbcd9181aa40e20ee6123d
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-28 21:06:42 -0500

    change: gave booking form its own page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
-import BookingForm from "@/components/BookingForm";
+import BookingForm from "@/app/dashboard/components/BookingForm";
+import Header from "@/components/Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <main>
         <h1>Welcome</h1>
-        <BookingForm />
+        <ul>
+          <li>
+            <Link href="/dashboard/book/booking-form">Book an Appointment</Link>
+          </li>
+          <li>
+            <Link href="/dashboard/book/booking-list">View Your Bookings</Link>
+          </li>
+        </ul>
       </main>
     </>
   );

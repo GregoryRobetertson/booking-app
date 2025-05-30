c6c2275844283c3eb4a692e607c0c160029b2e08
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-30 11:37:10 -0500

    add: 'use client'

--- src/app/dashboard/book/booking-form/page.jsx
+++ src/app/dashboard/book/booking-form/page.jsx
@@ -1,5 +1,6 @@
+"use client";
 import React from "react";
-import Header from "@/components/Header"; // Keep this if Header is in src/components/
+import Header from "@/components/Header";
 import BookingForm from "../../components/BookingForm";
 
 export default function page() {
--- src/app/dashboard/book/booking-list/page.jsx
+++ src/app/dashboard/book/booking-list/page.jsx
@@ -1,6 +1,13 @@
+"use client";
 import React from "react";
 import BookingList from "../../components/BookingList";
+import Header from "@/components/Header";
 
 export default function page() {
-  return <BookingList />;
+  return (
+    <>
+      <Header />
+      <BookingList />
+    </>
+  );
 }

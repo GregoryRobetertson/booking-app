1194847f4acc98b6223fdd363685a1391015a496
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-06-04 07:41:45 -0500

    added dont have an account and already have an account

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -88,19 +88,22 @@ export default function Login() {
         <button
           type="submit"
           disabled={loading}
-          className="bg-blue-600 hover:bg-blue-950 px-4 py-2"
+          className="bg-blue-600 hover:bg-blue-950 px-4 py-2 "
         >
           {loading ? "Loging In..." : "Log In"}
         </button>
         <button
           onClick={handleGoogleSignin}
           disabled={loading}
-          className="flex items-center justify-center gap-2 w-full max-w-xs border border-gray-300 rounded-md bg-white hover:bg-gray-50 px-4 py-2 shadow-sm disabled:opacity-50 disabled:cursor-not-allowed transition"
+          className="flex items-center justify-center gap-2 w-full max-w-xs border border-gray-300 rounded-md bg-white hover:bg-gray-400 px-4 py-2 shadow-sm disabled:opacity-50 disabled:cursor-not-allowed transition"
         >
           <span className="text-gray-700 font-medium text-sm">
             {loading ? "Signing in..." : "Sign in with Google"}
           </span>
         </button>
+        <a href="/auth/signup" className="hover:text-blue-800">
+          Don't have an account ? Create account
+        </a>
       </form>
 
       {error && <p style={{ color: "red" }}>{error}</p>}
--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -102,12 +102,15 @@ export default function SignupPage() {
         <button
           onClick={handleGoogleSignup}
           disabled={loading}
-          className="flex items-center justify-center gap-2 w-full max-w-xs border border-gray-300 rounded-md bg-white hover:bg-gray-50 px-4 py-2 shadow-sm disabled:opacity-50 disabled:cursor-not-allowed transition"
+          className="flex items-center justify-center gap-2 w-full max-w-xs border border-gray-300 rounded-md bg-white hover:bg-gray-400 px-4 py-2 shadow-sm disabled:opacity-50 transition"
         >
-          <span className="text-gray-700 font-medium text-sm">
+          <span className="text-gray-700 font-medium text-sm ">
             {loading ? "Signing in..." : "Sign in with Google"}
           </span>
         </button>
+        <a href="/auth/login" className="hover:text-blue-800">
+          Have an account already ? Login here
+        </a>
       </form>
 
       {error && <p style={{ color: "red" }}>{error}</p>}

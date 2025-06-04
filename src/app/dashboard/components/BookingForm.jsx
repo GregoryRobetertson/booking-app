845e7910da24bd469fb52dbd3d6d6c08e98dbd4f
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-06-04 12:08:33 -0500

    add styles

--- src/app/dashboard/components/BookingForm.jsx
+++ src/app/dashboard/components/BookingForm.jsx
@@ -54,34 +54,39 @@ export default function BookingForm() {
   };
 
   return (
-    <div>
-      <h1>Book Appointment</h1>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {success && <p style={{ color: "green" }}>{success}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Service Type</label>
+    <div className="flex justify-center items-center min-h-screen  bg-gray-300 px-4">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md bg-white rounded-2xl p-8 space-y-5 shadow-md w-full"
+      >
+        <h1 className="text-xl font-bold">Book Appointment</h1>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {success && <p style={{ color: "green" }}>{success}</p>}
+        <div className="space-y-2">
+          <label className="block  text-black font-medium">Service Type</label>
           <input
             type="text"
             name="serviceType"
             value={formData.serviceType ?? ""}
             onChange={handleChange}
             required
+            className="w-full border rounded px-4 py-2 resize-none border-black"
           />
         </div>
-        <div>
-          <label>Date</label>
+        <div className="space-y-2">
+          <label className="block  text-black font-medium">Date</label>
           <input
             type="date"
             name="date"
             value={formData.date ?? ""}
             onChange={handleChange}
             required
+            className="w-full border rounded px-4 py-2 resize-none border-black"
           />
         </div>
 
-        <div>
-          <label>Time Slot</label>
+        <div className="space-y-2">
+          <label className="block  text-black font-medium">Time Slot</label>
           <input
             type="text"
             name="timeSlot"
@@ -89,18 +94,28 @@ export default function BookingForm() {
             value={formData.timeSlot ?? ""}
             onChange={handleChange}
             required
+            className="w-full border rounded px-4 py-2 resize-none border-black"
           />
         </div>
-        <div>
-          <label> Notes (optional)</label>
+        <div className="space-y-2">
+          <label className="block  text-black font-medium">
+            {" "}
+            Notes (optional)
+          </label>
           <textarea
             name="notes"
             value={formData.notes ?? ""}
             onChange={handleChange}
             rows="4"
+            className="w-full border rounded px-4 py-2 resize-none border-black text-black font-medium"
           />
         </div>
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          className=" bg-blue-800 text-white px-4 py-2 rounded-3xl cursor-pointer hover:bg-blue-400"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );

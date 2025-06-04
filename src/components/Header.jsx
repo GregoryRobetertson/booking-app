c9aa2879b59d03cf4007709afc00e291ed954f9d
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-06-04 07:41:23 -0500

    change user display name color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,7 @@ export default function Header() {
             </>
           ) : (
             <>
-              <span className="text-sm text-gray-200">
+              <span className="text-sm text-black">
                 {user.displayName || user.email}
               </span>
               {user && (
